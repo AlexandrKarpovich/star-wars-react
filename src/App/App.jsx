d3c6a78f92d699a3d92ce56bb0500da0f805a3bd
alex karpovich <alieksandr.karpovich@mail.ru>
2022-09-19 10:45:43 +0300

    method fetch api

---
+++ src/App/App.jsx
@@ -0,0 +1,16 @@
+// import cn from 'classnames';
+import styles from './App.module.css';
+
+// console.log(styles);
+// import { getApiResource } from "../utils/network";
+import { getApiResource } from '../utils/network';
+
+// getApiResource();
+
+const App = () => {
+  return (
+    <h1 className={styles.header}>Hello</h1>
+  )
+}
+
+export default App;
